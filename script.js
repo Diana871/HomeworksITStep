3bf33e989c0be753c1225fed15f33642079d3ff6
Diana Horyacha <horiacha871@gmail.com>
2021-12-17 21:42:03 +0200

    Homework 20.12

---
+++ script.js
@@ -0,0 +1,31 @@
+// 6
+// const eurRate = 1.13;
+// let usd = prompt('Enter amount dollar');
+// let eurAmount = usd / eurRate;
+// alert('Amount in euro is ' +eurAmount); 
+
+// 7
+// const fileMb = 820;
+// let volumeGb = prompt('Entet flash drive volume, Gb');
+// let numberFiles = volumeGb * 1000 / fileMb;
+// alert('Number of files on flash drive is ' + numberFiles);
+
+// 8
+// let amountWallet = prompt('Enter amount of money in wallet, grn');
+// let priceChocolate = prompt('Enter price of one chocolate, grn');
+// let numberChocolate = Math.floor(amountWallet / priceChocolate);
+// let rest = amountWallet - (numberChocolate * priceChocolate);
+// alert('Number of chocolates you can buy is ' +numberChocolate);
+// alert('The rest of money is ' + rest + ' grn');
+
+// 9
+// let x = prompt('Enter three-digit number');
+// let y = (x % 10)*100 + x % 100 - (x % 10)%10 + (x - x % 100)/100;
+// alert(y);
+
+// 10
+// let x = prompt('Enter integer');
+// let y = x % 2;
+// y==1 && (y='odd');
+// y==0 && (y='even');
+// alert(y);
